4e0cdf032252f0b1746cb06d66b0b287d2e6cc6e
AustinHodgin <austinhodgin@yahoo.com>
2020-06-02 15:19:50 -0700

    added js to path_display.js to build the grid

---
+++ js/path_display.js
@@ -0,0 +1,25 @@
+let GRID_ROWS = 20;
+let GRID_COLS = 20;
+
+function createGrid() {
+  let grid = document.getElementById("grid");
+  let tablebody = document.createElement("tbody");
+
+  for (let i = 0; i <= GRID_ROWS; i++) {
+    let row = document.createElement("tr");
+
+    for (let j = 0; j <= GRID_COLS; j++) {
+      let cell = document.createElement("td");
+      cell.setAttribute("id", i + " " + j);
+      //let cellText = document.createTextNode(i + " " + j);
+      let cellText = document.createTextNode(" ");
+      cell.appendChild(cellText);
+      row.appendChild(cell);
+    }
+    tablebody.appendChild(row);
+  }
+  grid.appendChild(tablebody);
+  grid.setAttribute("border", 2);
+}
+
+createGrid();
