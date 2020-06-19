e98a8bb436d5f79d6ce860872b86f7964e8e90d3
AustinHodgin <austinhodgin@yahoo.com>
2020-06-18 17:41:04 -0700

    Added ability to click on cells and turn them into walls

--- js/path.js
+++ js/path.js
@@ -17,11 +17,11 @@ let toggleStart = false;
 let toggleEnd = false;
 
 /*Color Definitions*/
-let DEFAULT_COLOR;
-let VISITED_COLOR;
-let WALL_COLOR;
-let START_COLOR;
-let END_COLOR;
+let DEFAULT_COLOR = "white";
+let VISITED_COLOR = "yellow";
+let WALL_COLOR = "gray";
+let START_COLOR = "green";
+let END_COLOR = "red";
 let PATH_COLOR;
 
 /* Default start and end i and j*/
@@ -35,25 +35,33 @@ class Cell {
     this.cellID = cellID;
     this.htmlCell = htmlCell;
     this.setType(type, htmlCell);
+    this.viewed = false;
   }
 
   setType(type, htmlCell) {
     switch (type) {
       case "START":
         this.type = "START";
-        htmlCell.style.backgroundColor = "green";
+        htmlCell.style.backgroundColor = START_COLOR;
         break;
       case "END":
         this.type = "END";
-        htmlCell.style.backgroundColor = "red";
+        htmlCell.style.backgroundColor = END_COLOR;
         break;
       case "WALL":
         this.type = "WALL";
-        htmlCell.style.backgroundColor = "gray";
+        htmlCell.style.backgroundColor = WALL_COLOR;
+        break;
+      case "VISITED":
+        this.viwed = true;
+        htmlCell.sytle.backgroundColor = VISITED_COLOR;
+        break;
+      case "Path":
+        htmlCell.sytle.backgroundColor = PATH_COLOR;
         break;
       default:
         this.type = "DEFAULT";
-        htmlCell.style.backgroundColor = "white";
+        htmlCell.style.backgroundColor = DEFAULT_COLOR;
     }
   }
 }
@@ -65,23 +73,54 @@ class Grid {
     this.grid = new Array(GRID_SIZE)
       .fill(0)
       .map(() => new Array(GRID_SIZE).fill(0));
+    this.startID = 0;
+    this.endID = 0;
   }
   crateGrid() {
     for (let i = 0; i < GRID_SIZE; i++) {
       for (let j = 0; j < GRID_SIZE; j++) {
+        let cellID = `${i} ${j}`;
+        let htmlCell = document.getElementById(cellID);
+
         if (i === startI && j === startJ) {
-          let cellID = `${i} ${j}`;
-          let htmlCell = document.getElementById(cellID);
           this.grid[i][j] = new Cell(cellID, "START", htmlCell);
+          this.startID = cellID;
         } else if (i === endI && j === endJ) {
-          let cellID = `${i} ${j}`;
-          let htmlCell = document.getElementById(cellID);
           this.grid[i][j] = new Cell(cellID, "END", htmlCell);
+          this.endID = cellID;
         } else {
-          let cellID = `${i} ${j}`;
-          let htmlCell = document.getElementById(cellID);
           this.grid[i][j] = new Cell(cellID, "", htmlCell);
         }
+      }
+    }
+  }
+
+  toggleCellWall(cell, gridObj) {
+    let type = cell.type;
+
+    let grid = gridObj.grid;
+    let startID = gridObj.startID.split(" ");
+    let endID = gridObj.endID.split(" ");
+
+    switch (type) {
+      case "DEFAULT":
+        cell.setType("WALL", cell.htmlCell);
+        break;
+      case "WALL":
+        cell.setType("Defualt", cell.htmlCell);
+        break;
+    }
+  }
+
+  addOnClick() {
+    for (let i = 0; i < GRID_SIZE; i++) {
+      for (let j = 0; j < GRID_SIZE; j++) {
+        this.grid[i][j].htmlCell.onclick = function (event) {
+          let id = event.target.id.split(" ");
+          let i = parseInt(id[0]);
+          let j = parseInt(id[1]);
+          this.toggleCellWall(this.grid[i][j], this);
+        }.bind(this);
       }
     }
   }
@@ -90,3 +129,4 @@ class Grid {
 let firstGrid = new Grid();
 
 firstGrid.crateGrid();
+firstGrid.addOnClick();
