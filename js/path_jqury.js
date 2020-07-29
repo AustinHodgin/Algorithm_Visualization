984f03125dc9f4eae7909dede78cca1519671070
AustinHodgin <austinhodgin@yahoo.com>
2020-07-29 16:21:11 -0700

    Added summary and and psudo code sections to the web page for both breath first and depth first.

--- js/path_jqury.js
+++ js/path_jqury.js
@@ -16,31 +16,22 @@ $("#algo").change(function () {
   let summaryText = "";
   let codeText = "";
   let complexityText = "";
-  //   if (selectedAlgo === "bubble") {
-  //     summaryText =
-  //       "Bubble sort is a simple sorting algorithm that looks at the first element and compares it to the element to the right. If it is larger then it will swap the two elements and then move on to the next element. This compairison is done until it finds an element smaller then it. It will then start over at the begining. This proccess is repeted until the arry is sorted.";
-  //     codeText =
-  //       "BubbleSort(array): \n  for i to n: \n    for j to n-i-1: \n     if array[j] > arr[j+1] \n      swap(array[j] ,array[j+1])";
-  //     complexityText = "Complexity: O(n^2)";
-  //   } else if (selectedAlgo === "selection") {
-  //     summaryText =
-  //       "Selection sort sorts by repeatedly finding the minimum element from the array and putting it at the beggining. It will then move the begining point up one. This is repeated until the arry is sorted.";
-  //     codeText =
-  //       "SelectionSort(array): \n   for i = 1 to n - 1: \n     min = i \n     for j = i+1 to n: \n       if array[j] < list[min] then \n           min = j \n        if indexMin != i then \n          swap(list[min], list[i]";
-  //     complexityText = "Complexity: O(n^2)";
-  //   } else if (selectedAlgo === "merge") {
-  //     summaryText =
-  //       "Merge sort is a divide and conquer algorithm that divides the array into several sub arrays, sorts those, and then merges them back together. This algorithm is a recrivice algorithm which helps cut down the run time. ";
-  //     codeText =
-  //       "MergeSort(array, left, right): \n    if left > right\n     return\n    mid = (left + right)/2\n    MergeSort(array, left, mid);\n    MergeSort(array, mid+1, right);\n    Merge(array, left, mid, right)";
-  //     complexityText = "Complexity: O(nlog(n))";
-  //   } else if (selectedAlgo === "insertion") {
-  //     summaryText =
-  //       "Insertion sort builds a final array one item at a time. It iterates through the array looking for where the element should be inserted and insertis it into the correct place within the final array. This proccess is repeated until all the elements have been placed in the correct place.";
-  //     codeText =
-  //       "InsertionSort(array): \n   for j = 2 to n: \n     key = array[j] \n      insert array[j] \n     i = j - 1\n     while i > 0 and array[i] > key:\n       do array[i+1] = array[i]\n        i = i -1 \n     array[i+1] = key";
-  //     complexityText = "Complexity: O(n^2)";
-  //   }
+  if (selectedAlgo === "BreathFirst") {
+    summaryText =
+      "Breadth First Search is a search algorithm that traverse a graph looking for a given node. This algorithm starts at a given node and will then traverse the graph going layer by layer looking from left to right for the node.This algorithm is very similar to Depth First Search but instead of looking at the end of the search arry it looks at the start of it. If there is a path to from the start node to the end node it will guarantee it will find it and find the shortest path to it.";
+    codeText =
+      'BFS(Graph, startNode) {  \n \t let search = [ ]; \n \t let path = [ ];\n \t search.push(StartNode);\n \t while(search.length != 0) { \n \t \t  let node = search.shift(); \n \t \t   path.push(node); \n \t \t   if (node.type === "DEFAULT") { \n \t \t \t  node.setType("VISITED"); \n \t \t } \n \t \t if (node.type === "END") { \n \t \t \t let finalPath = backTrace(node); \n \t \t \t displayPath(finalPath); \n \t \t } else { \n \t \t \t  let neighbors = findNeighbors(node, search); \n \t \t \t  search = search.concat(neighbors); \n \t \t \t} \n \t \t if (search.length === 0) { \n \t \t \t print("No path found"); \n \t \t  } \n \t  }\n';
+    complexityText =
+      "Time Complexity: O( V + E) Where V is number of nodes and E is number of edges ";
+  } else if (selectedAlgo === "DepthFirst") {
+    summaryText =
+      "Deapth first search is a recursive algorithm that searches a graph by going down the left most path and then once it reaches the end and then will go back up the tree until it can go right and will repeat the proccess until it finds the end node. This algorithm is very similar to Breadth First Search but instead of looking at the front of the search arry it looks at the end of it.  This will garaentee a path to the end node from the start node if one exists but does not garaentee the shortest path.   ";
+    codeText =
+      'DFS(Graph, startNode) {  \n \t let search = [ ]; \n \t let path = [ ];\n \t search.push(StartNode);\n \t while(search.length != 0) { \n \t \t  let node = search.pop(); \n \t \t   path.push(node); \n \t \t   if (node.type === "DEFAULT") { \n \t \t \t  node.setType("VISITED"); \n \t \t } \n \t \t if (node.type === "END") { \n \t \t \t let finalPath = backTrace(node); \n \t \t \t displayPath(finalPath); \n \t \t } else { \n \t \t \t  let neighbors = findNeighbors(node, search); \n \t \t \t  search = search.concat(neighbors); \n \t \t \t} \n \t \t if (search.length === 0) { \n \t \t \t print("No path found"); \n \t \t  } \n \t  }\n';
+
+    complexityText =
+      "Time Complexity: O( V + E) Where V is number of nodes and E is number of edges";
+  }
   $("#summary").text(summaryText);
   $("#complexity").text(complexityText);
   $("#code").text(codeText);
